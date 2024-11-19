d232550ccb81a1204846936e78ffe76dd6263868
jongddak <hfsmsj316@gmail.com>
2024-11-19 11:20:57 +0900

    1번 문제 풀이

--- Q_01/Assets/Scripts/CubeController.cs
+++ Q_01/Assets/Scripts/CubeController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
-    public Vector3 SetPoint { get; private set; }
+    public Vector3 SetPoint { get; set; }
 
+
+    private void Start()
+    {
+        Debug.Log(SetPoint.x);
+    }
     public void SetPosition()
     {
+        Debug.Log("셋포지션");
         transform.position = SetPoint;
     }
 }
